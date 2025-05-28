b673589bffcb46fa0e3d68bf59e975d32c1ab4d0
wp <97200483+weijiePan@users.noreply.github.com>
2025-05-27 21:14:42 -0400

    loading font-background

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import "./index.css";
 import { losAngeles, chicago } from "./restaurant";
 let leftDisplay = document.getElementById("left-store-display");
 let rightDisplay = document.getElementById("right-store-display");
-
-function loadStoreDisplay(establishment, display, background){
-    display.querySelector('p.location').textContent = establishment.city;
-    display.querySelector('p.name').textContent = establishment.name;
-    display.style.backgroundImage = `url(${background})`;
+import morningRestaurant  from "../asset/morningRestaurant.jpg";
+import nightTimeRestaurant from "../asset/nightTimeRestaurant.jpg";
+import squareBackground from "../asset/font-background/square(1).jpg";
+import circleBackground from "../asset/font-background/circle3.png";
+function loadStoreDisplay(establishment, display, background, fontBackground){
+    display.querySelector("img").setAttribute("src", fontBackground);
 }
-loadStoreDisplay(losAngeles, leftDisplay, "../asset/morningRestaurant.jpg");
-loadStoreDisplay(chicago, rightDisplay, "../asset/nightTimeRestaurant.jpg");
+loadStoreDisplay(losAngeles, leftDisplay, morningRestaurant, squareBackground);
+loadStoreDisplay(chicago, rightDisplay, nightTimeRestaurant, circleBackground);

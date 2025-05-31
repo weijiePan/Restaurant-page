24351ff6375f3411732ca9c062bb3eb195520b47
wp <97200483+weijiePan@users.noreply.github.com>
2025-05-31 14:31:08 -0400

    main page change

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import morningRestaurant  from "../asset/morningRestaurant.jpg";
 import nightTimeRestaurant from "../asset/nightTimeRestaurant.jpg";
 import squareBackground from "../asset/font-background/square(1).jpg";
 import circleBackground from "../asset/font-background/circle3.png";
-function loadStoreDisplay(establishment, display, background, fontBackground){
-    display.querySelector("img").setAttribute("src", fontBackground);
+function loadStoreDisplay(establishment, display, bg, fontBackground){
+    display.querySelector("p.name").textContent = establishment.name;
+    display.querySelector("p.city").textContent = establishment.city;
+    display.style.background = `url(${bg})`;
 }
 loadStoreDisplay(losAngeles, leftDisplay, morningRestaurant, squareBackground);
 loadStoreDisplay(chicago, rightDisplay, nightTimeRestaurant, circleBackground);
